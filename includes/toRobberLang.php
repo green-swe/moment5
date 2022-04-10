3b1ed1c1717b47bebed4c116396daa85408746f4
David Green <ejfimp@gmail.com>
2022-04-10 20:57:24 +0200

    refined the function

--- includes/toRobberLang.php
+++ includes/toRobberLang.php
@@ -1,16 +1,10 @@
 <?php function toRobberLang($string)
 {
-  $vowels = array('a', 'e', 'i', 'o', 'u', 'y', 'å', 'ä', 'ö');
-  $specialCharacters = '/[\'^£$%&*()}{@#~?!><>,|=_+¬-]/';
+  $consonants = '/[b-df-hj-np-tv-z]/i';
   $split = preg_split('//u', $string);
-  //preg splits adds empty data at the start and end of the array,
-  //these needs to be removed
-  array_shift($split);
-  array_pop($split);  
 
   for ($i = 0; $i < count($split); $i++) {
-    
-    if (!in_array($split[$i], $vowels) && !ctype_space($split[$i]) && !preg_match($specialCharacters, $split[$i])) {
+    if (preg_match($consonants, $split[$i])) {
       $join = $split[$i] . "o" . strtolower($split[$i]);
       $split[$i] = $join;
       echo $split[$i] . "<br>";
